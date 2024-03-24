a654aa7e4265e19688570f41c2b9e43cf67c8115
Shant Dashjian <shantgdashjian@gmail.com>
2024-03-24 18:34:29 +0400

    Add initial project and Netty config

---
+++ src/test/java/com/cloudnativebookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudnativebookshop.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
